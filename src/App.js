c5a38638b9c6f5153c2e7e3e2c894d79c6677f31
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2022-10-24 20:18:35 -0300

    Revert "[Grupo-16] Requisitos 1, 3, 4 e 5"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import Login from './components/Login';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Provider from './contextApi/Provider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">Grupo-16</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-      <Login />
-    </div>      
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
