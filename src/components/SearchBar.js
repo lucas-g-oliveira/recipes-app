0f4d10262ad8beda40c4afa8474d0e621296a52e
Fa-biano <fbiano.souza@gmail.com>
2022-10-25 17:52:18 -0300

    realizado o requisito 11

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../contextApi/AppContext';
 
 function SearchBar() {
+  const { handleSearchChange, handleClickApi } = useContext(AppContext);
+
   return (
     <div>
-      <input data-testid="search-input" type="text" />
       <input
-        type="radio"
-        data-testid="ingredient-search-radio"
-        value="ingredientes"
-        name="option"
+        data-testid="search-input"
+        type="text"
+        name="valueToSearch"
+        onChange={ handleSearchChange }
       />
-      <input
-        type="radio"
-        data-testid="name-search-radio"
-        value="Name"
-        name="option"
-      />
-      <input
-        type="radio"
-        value="Primeira letra"
-        data-testid="first-letter-search-radio"
-        name="option"
-      />
-      <button type="submit" data-testid="exec-search-btn">Buscar</button>
+      <label htmlFor="ingredientes">
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          value="ingredientes"
+          name="option"
+          id="ingredientes"
+          onChange={ handleSearchChange }
+        />
+        Ingredientes
+      </label>
+      <label htmlFor="name">
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          value="name"
+          name="option"
+          id="name"
+          onChange={ handleSearchChange }
+        />
+        Nome
+      </label>
+      <label htmlFor="first-letter">
+        <input
+          type="radio"
+          value="primeiraLetra"
+          data-testid="first-letter-search-radio"
+          name="option"
+          id="first-letter"
+          onChange={ handleSearchChange }
+        />
+        Primeira letra
+      </label>
+      <button
+        type="submit"
+        data-testid="exec-search-btn"
+        onClick={ handleClickApi }
+      >
+        Buscar
+      </button>
     </div>
   );
 }
