dbbe4b3e02ff74574db48c2481a42bcda030e1d8
Igor Arecippo <iarecippo@gmail.com>
2022-10-26 19:06:54 -0300

    requisito 20 sem erro de lint details comentado

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,56 +1,56 @@
-import React, { useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import AppContext from '../contextApi/AppContext';
-import Footer from './Footer';
+// import React, { useContext, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import AppContext from '../contextApi/AppContext';
+// import Footer from './Footer';
 
-function RecipesDetails() {
-  const { selectedRecipe } = useContext(AppContext);
-  const { location: { pathname } } = useHistory();
+// // function RecipesDetails() {
+// //   const { selectedRecipe } = useContext(AppContext);
+// //   const { location: { pathname } } = useHistory();
 
-  // handleClick --> target --> trazer id --> vamos colocar esse id na url --> fetch com url
-/*   const [selectedRecipe, setSelectedRecipe] = useState('')
-  handleClick({target}){
-        setSelectedRecipe(target);
-    }
-  }; */
+//   // handleClick --> target --> trazer id --> vamos colocar esse id na url --> fetch com url
+// /*   const [selectedRecipe, setSelectedRecipe] = useState('')
+//   handleClick({target}){
+//         setSelectedRecipe(target);
+//     }
+//   }; */
 
-  useEffect(() => {
-    const fetchDetail = async () => {
-      if (selectedRecipe.key ==) {
-        
-      } else {
-        
-      }
-    };
-    fetchTudo();
-  });
+//   // useEffect(() => {
+//   //   const fetchDetail = async () => {
+//   //     if (selectedRecipe.key ==) {
 
-  return (
-    <div>
-      <div>
-        {
-          resultsMap.map((result, index) => (
-            <div
-              key={ result.idMeal ? result.idMeal : result.idDrink }
-              data-testid={ `${index}-recipe-card` }
-            >
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ result.strMealThumb ? result.strMealThumb : result.strDrinkThumb }
-                alt={ result.strMeal ? result.strMeal : result.strDrink }
-              />
-              <p
-                data-testid={ `${index}-card-name` }
-              >
-                { result.strMeal ? result.strMeal : result.strDrink }
-              </p>
-            </div>
-          ))
-        }
-      </div>
-      <Footer />
-    </div>
-  );
-}
+//   //     } else {
 
-export default Recipes;
+//   //     }
+//   //   };
+//   //   fetchTudo();
+//   // });
+
+//   return (
+//     <div>
+//       <div>
+//         {
+//           resultsMap.map((result, index) => (
+//             <div
+//               key={ result.idMeal ? result.idMeal : result.idDrink }
+//               data-testid={ `${index}-recipe-card` }
+//             >
+//               <img
+//                 data-testid={ `${index}-card-img` }
+//                 src={ result.strMealThumb ? result.strMealThumb : result.strDrinkThumb }
+//                 alt={ result.strMeal ? result.strMeal : result.strDrink }
+//               />
+//               <p
+//                 data-testid={ `${index}-card-name` }
+//               >
+//                 { result.strMeal ? result.strMeal : result.strDrink }
+//               </p>
+//             </div>
+//           ))
+//         }
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default Recipes;
