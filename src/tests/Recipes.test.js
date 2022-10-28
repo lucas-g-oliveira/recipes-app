acaa6e5ead1624fc03dac45bb5ffd3a9d75951d1
Lucas G <lucasg.devmail93@gmail.com>
2022-10-28 12:50:38 -0300

    cobertura-de-testes-de 45% do req-20 -> Ok

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -7,46 +7,12 @@ import App from '../App';
 // import Meals from '../pages/Meals';
 // import meals from '../../cypress/mocks/meals';
 import mealCategories from '../../cypress/mocks/mealCategories';
+import beefMeals from '../../cypress/mocks/beefMeals';
 
 describe('Testa o componente Recipes', () => {
   it('Verifica renderizacao dos filtros por categoria', async () => {
-    // global.fetch = jest.fn(() => Promise.resolve({
-    //   json: () => Promise.resolve(mealCategories),
-    // }));
-    /* await act(async () => { renderWithRouter(<App />); }); */
     const { history } = renderWithRouter(<App />);
 
-    // const email = screen.getByTestId('email-input');
-    // const password = screen.getByTestId('password-input');
-    // const btnSubmit = screen.getByTestId('login-submit-btn');
-
-    // userEvent.type(email, 'test@test.com');
-    // userEvent.type(password, '1234567');
-
-    // expect(btnSubmit).toBeEnabled();
-    // userEvent.click(btnSubmit);
-
-    // await act(async () => history.push('/meals'));
-    // const { location: { pathname } } = history;
-
-    // const filterBeef = screen.findByTestId('Beef-category-filter');
-    /*  const filterBreackFast = screen.findByTestId('Breakfast-category-filter');
-    const filterChicken = screen.findByTestId('Chicken-category-filter');
-    const filterDessert = screen.findByTestId('Dessert-category-filter');
-    const filterGoal = screen.findByTestId('Goat-category-filter');
-    const filterAll = screen.findByTestId('All-category-filter');
-
-    expect(filterBeef).toBeDefined();
-    expect(filterBreackFast).toBeDefined();
-    expect(filterChicken).toBeDefined();
-    expect(filterDessert).toBeDefined();
-    expect(filterGoal).toBeDefined();
-    expect(filterAll).toBeDefined();
-
-    userEvent.click(filterBeef);
-
-    const firstRecipe = screen.findByTestId('0-card-name');
-    expect(firstRecipe).toHaveTextContent(/beef and mustard pie/i); */
     const email = screen.getByTestId('email-input');
     expect(email).toBeEnabled();
     userEvent.type(email, 'test@test.com');
@@ -67,105 +33,27 @@ describe('Testa o componente Recipes', () => {
       json: () => Promise.resolve(mealCategories),
     }));
 
-    /* it('mock setTimeout test', () => {
-      jest.useFakeTimers();
-      setTimeout(() => {
-        expect(component.state().fruits).toEqual(fruits);
-      }, 1500);
-      jest.runAllTimers();
-     }); */
-
-    const filterBeef = await screen.findByTestId('Beef-category-filter');
-    // console.log(filterBeef);
-    expect(filterBeef).toBeInTheDocument();
-    // expect(filterBeef).toBeInTheDocument();
-
     const filterAll = screen.getByTestId('All-category-filter');
+    const filterBeef = await screen.findByTestId('Beef-category-filter');
+    const filterBreackFast = await screen.findByTestId('Breakfast-category-filter');
+    const filterChicken = await screen.findByTestId('Chicken-category-filter');
+    const filterDessert = await screen.findByTestId('Dessert-category-filter');
+    const filterGoal = await screen.findByTestId('Goat-category-filter');
+
     expect(filterAll).toBeInTheDocument();
+    expect(filterBeef).toBeInTheDocument();
+    expect(filterBreackFast).toBeDefined();
+    expect(filterChicken).toBeDefined();
+    expect(filterDessert).toBeDefined();
+    expect(filterGoal).toBeDefined();
 
-    // global.fetch.mockClear();
+    userEvent.click(filterBeef);
+
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(beefMeals),
+    }));
+
+    const firstRecipe = await screen.findByRole('img', { name: /corba/i });
+    expect(firstRecipe).toBeInTheDocument();
   });
-
-  /* // it.skip('Verifica renderizacao dos filtros por categoria na pagina', async () => {
-  //   // global.fetch = jest.fn(() => Promise.resolve({
-  //   //   json: () => Promise.resolve(mealCategories),
-  //   // }));
-  //   await act(async () => { renderWithRouter(<App />); });
-  //   const { history } = renderWithRouter(<App />);
-  //
-  //   // const email = screen.getByTestId('email-input');
-  //   // const password = screen.getByTestId('password-input');
-  //   // const btnSubmit = screen.getByTestId('login-submit-btn');
-
-  //   // userEvent.type(email, 'test@test.com');
-  //   // userEvent.type(password, '1234567');
-
-  //   // expect(btnSubmit).toBeEnabled();
-  //   // userEvent.click(btnSubmit);
-
-  //   // await act(async () => history.push('/meals'));
-  //   // const { location: { pathname } } = history;
-
-  //   // const filterBeef = screen.findByTestId('Beef-category-filter');
-  //   const filterBreackFast = screen.findByTestId('Breakfast-category-filter');
-  //   const filterChicken = screen.findByTestId('Chicken-category-filter');
-  //   const filterDessert = screen.findByTestId('Dessert-category-filter');
-  //   const filterGoal = screen.findByTestId('Goat-category-filter');
-  //   const filterAll = screen.findByTestId('All-category-filter');
-
-  //   expect(filterBeef).toBeDefined();
-  //   expect(filterBreackFast).toBeDefined();
-  //   expect(filterChicken).toBeDefined();
-  //   expect(filterDessert).toBeDefined();
-  //   expect(filterGoal).toBeDefined();
-  //   expect(filterAll).toBeDefined();
-
-  //   userEvent.click(filterBeef);
-
-  //   const firstRecipe = screen.findByTestId('0-card-name');
-  //   expect(firstRecipe).toHaveTextContent(/beef and mustard pie/i);
-  //   const email = screen.getByTestId('email-input');
-  //   expect(email).toBeEnabled();
-  //   userEvent.type(email, 'test@test.com');
-
-  //   const password = screen.getByTestId('password-input');
-  //   expect(password).toBeEnabled();
-  //   userEvent.type(password, 'test@test.com');
-
-  //   const btnSubmit = screen.getByTestId('login-submit-btn');
-  //   expect(btnSubmit).toBeEnabled();
-  //   userEvent.click(btnSubmit);
-
-  //   act(() => history.push('/drinks'));
-  //   const { location: { pathname } } = history;
-  //   expect(pathname).toBe('/drinks');
-
-  //   // global.fetch = jest.fn(() => Promise.resolve({
-  //   //   json: () => Promise.resolve(mealCategories),
-  //   // }));
-
-  //    it('mock setTimeout test', () => {
-  //     jest.useFakeTimers();
-  //     setTimeout(() => {
-  //       expect(component.state().fruits).toEqual(fruits);
-  //     }, 1500);
-  //     jest.runAllTimers();
-  //    });
-
-  //   // const filterBeef = screen.getByTestId('Beef-category-filter');
-  //   await waitForElement(() => expect(screen.getByTestId('Ordinary Drink-category-filter')).toBeInTheDocument());
-  //   // expect(filterBeef).toBeInTheDocument();
-
-  //   const filterAll = screen.getByTestId('All-category-filter');
-  //   expect(filterAll).toBeInTheDocument();
-
-  //   global.fetch.mockClear();
-  // });
-
-  // it('teste', () => {
-  //   renderWithRouter(<Meals option="meals" />);
-
-  //   const filterAll = screen.getByTestId('All-category-filter');
-  //   expect(filterAll).toBeInTheDocument();
-  // }); */
 });
