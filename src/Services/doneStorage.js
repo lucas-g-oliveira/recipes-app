3effb734a8f04c646854c66334b366df547dd9b1
Ester Viana Verza <ester.av@hotmail.com>
2022-10-28 18:18:37 -0300

    arrumando lint

--- src/Services/doneStorage.js
+++ src/Services/doneStorage.js
@@ -1,5 +1,17 @@
-export const doneRecipe = (recipe) => {
-  localStorage.setItem('doneRecipes', JSON.stringify({
-    id: recipe.idMeal
-  }))
-}
+// const doneRecipe = (recipe) => {
+//   localStorage.setItem('doneRecipes', JSON.stringify({
+//     id: 'id-da-receita',
+//     type: 'meal-ou-drink',
+//     nationality: 'nacionalidade-da-receita-ou-texto-vazio',
+//     category: 'categoria-da-receita-ou-texto-vazio',
+//     alcoholicOrNot: 'alcoholic-ou-non-alcoholic-ou-texto-vazio',
+//     name: 'nome-da-receita',
+//     image: 'imagem-da-receita',
+//     doneDate: 'quando-a-receita-foi-concluida',
+//     tags: 'array-de-tags-da-receita-ou-array-vazio',
+//   }));
+// };
+
+// export const
+
+// export default doneRecipe;
