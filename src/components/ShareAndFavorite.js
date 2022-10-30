3e224325272f7c446b9bbece5f117e9948f41cd7
Fa-biano <fbiano.souza@gmail.com>
2022-10-29 21:08:47 -0300

    realizado o requisito 34 e 35

--- src/components/ShareAndFavorite.js
+++ src/components/ShareAndFavorite.js
@@ -1,16 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import AppContext from '../contextApi/AppContext';
+import { getFavorites, saveFavorites } from '../services/saveRecipe';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 const copy = require('clipboard-copy');
 
 function ShareAndFavorite() {
   const { location: { pathname } } = useHistory();
-  const [hasCopy, setHasCopy] = useState([false]);
+  const { selectedRecipe } = useContext(AppContext);
+
+  const [hasCopy, setHasCopy] = useState(false);
+  const [hasFavorite, setHasFavorite] = useState(false);
 
   const getCopiedLink = () => {
     copy(`http://localhost:3000${pathname}`);
     setHasCopy(true);
   };
+
+  const saveFavoriteRecipe = () => {
+    const favorites = getFavorites();
+
+    const recipe = {
+      id: selectedRecipe[0].idMeal ? selectedRecipe[0].idMeal : selectedRecipe[0].idDrink,
+      type: selectedRecipe[0].idMeal ? 'meal' : 'drink',
+      nationality: selectedRecipe[0].strArea
+        ? selectedRecipe[0].strArea
+        : '',
+      category: selectedRecipe[0].strCategory,
+      alcoholicOrNot: selectedRecipe[0].strAlcoholic
+        ? selectedRecipe[0].strAlcoholic
+        : '',
+      name: selectedRecipe[0].strMeal
+        ? selectedRecipe[0].strMeal
+        : selectedRecipe[0].strDrink,
+      image: selectedRecipe[0].strMealThumb
+        ? selectedRecipe[0].strMealThumb
+        : selectedRecipe[0].strDrinkThumb,
+    };
+
+    let updateFavorites;
+
+    if (favorites !== null) {
+      updateFavorites = [...favorites, recipe];
+    } else {
+      updateFavorites = [recipe];
+    }
+    saveFavorites(updateFavorites);
+  };
+
+  const checkFavoriteRecipe = useCallback(() => {
+    const favorites = getFavorites();
+    const pageType = pathname.includes('meals');
+    let recipeId;
+
+    if (pageType) {
+      recipeId = pathname.replace('/meals/', '');
+    } else {
+      recipeId = pathname.replace('/drinks/', '');
+    }
+
+    if (favorites !== null) {
+      const checkFavorite = favorites.some((recipe) => recipe.id === recipeId);
+      if (checkFavorite) {
+        setHasFavorite(checkFavorite);
+      }
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    checkFavoriteRecipe();
+  }, [checkFavoriteRecipe]);
 
   return (
     <div>
@@ -23,18 +84,28 @@ function ShareAndFavorite() {
       </button>
       <button
         type="button"
-        data-testid="favorite-btn"
+        onClick={ saveFavoriteRecipe }
+        src={ hasFavorite ? blackHeartIcon : whiteHeartIcon }
       >
-        Favorite
+        {
+          hasFavorite
+            ? <img src={ blackHeartIcon } alt="favorite" data-testid="favorite-btn" />
+            : <img src={ whiteHeartIcon } alt="favorite" data-testid="favorite-btn" />
+        }
       </button>
       {
-        hasCopy === true && (
+        hasCopy && (
           <div>
             <p>Link copied!</p>
             <button type="button" onClick={ () => setHasCopy(false) }>Ok</button>
           </div>
         )
       }
+      {/* {
+        hasFavorite
+          ? <img src={ blackHeartIcon } alt="favorite" />
+          : <img src={ whiteHeartIcon } alt="favorite" />
+      } */}
     </div>
   );
 }
