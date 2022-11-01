6d865826e0c076c333cee21a09ad1af944f35d1b
Ester Viana Verza <ester.av@hotmail.com>
2022-11-01 17:40:37 -0300

    pag profile

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -5,19 +5,25 @@ import App from '../App';
 import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('Testa a página de Profile', () => {
-  it('Verifica se O formulário só fica válido após um email válido e uma senha de mais de 6 caracteres', () => {
+  const emailId = 'email-input';
+  const passId = 'password-input';
+  const loginId = 'login-submit-btn';
+  const emailTest = 'test@test.com';
+  const passTest = '1234567';
+  const idProfile = 'profile-top-btn';
+  it('Verifica se a página de profile tem os elementos corretos', () => {
     const { history } = renderWithRouter(<App />);
-    const email = screen.getByTestId('email-input');
-    const password = screen.getByTestId('password-input');
-    const btnSubmit = screen.getByTestId('login-submit-btn');
+    const email = screen.getByTestId(emailId);
+    const password = screen.getByTestId(passId);
+    const btnSubmit = screen.getByTestId(loginId);
 
     expect(email).toBeInTheDocument();
     expect(password).toBeInTheDocument();
     expect(btnSubmit).toBeInTheDocument();
     expect(btnSubmit).toBeDisabled();
 
-    userEvent.type(email, 'test@test.com');
-    userEvent.type(password, '1234567');
+    userEvent.type(email, emailTest);
+    userEvent.type(password, passTest);
 
     expect(btnSubmit).toBeEnabled();
 
@@ -27,21 +33,83 @@ describe('Testa a página de Profile', () => {
     const { location: { pathname } } = history;
     expect(pathname).toBe('/meals');
 
-    const profileBtn = screen.getByTestId('profile-top-btn');
+    const profileBtn = screen.getByTestId(idProfile);
     expect(profileBtn).toBeInTheDocument();
     userEvent.click(profileBtn);
-
-    history.push('/meals');
-
-    const doneBtn = screen.getByTestId('profile-done-btn');
-    expect(doneBtn).toBeInTheDocument();
 
     const favoriteBtn = screen.getByTestId('profile-favorite-btn');
     expect(favoriteBtn).toBeInTheDocument();
 
+    userEvent.click(favoriteBtn);
+    /* const textFavorite = screen.getByText(/favorite/i);
+    expect(textFavorite).toBeInTheDocument(); */
+  });
+  it('Verifica se o botão done funciona', () => {
+    const { history } = renderWithRouter(<App />);
+    const email = screen.getByTestId(emailId);
+    const password = screen.getByTestId(passId);
+    const btnSubmit = screen.getByTestId(loginId);
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(btnSubmit).toBeInTheDocument();
+    expect(btnSubmit).toBeDisabled();
+
+    userEvent.type(email, emailTest);
+    userEvent.type(password, passTest);
+
+    expect(btnSubmit).toBeEnabled();
+
+    userEvent.click(btnSubmit);
+
+    history.push('/meals');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+
+    const profileBtn = screen.getByTestId(idProfile);
+    expect(profileBtn).toBeInTheDocument();
+    userEvent.click(profileBtn);
+
+    const doneBtn = screen.getByTestId('profile-done-btn');
+    expect(doneBtn).toBeInTheDocument();
+
+    userEvent.click(doneBtn);
+    /* act(() => history.push('/done-recipes')); */
+    const { location } = history;
+    expect(location.pathname).toBe('/done-recipes');
+  });
+  it('Verifica se o botão logout funciona', () => {
+    const { history } = renderWithRouter(<App />);
+    const email = screen.getByTestId(emailId);
+    const password = screen.getByTestId(passId);
+    const btnSubmit = screen.getByTestId(loginId);
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(btnSubmit).toBeInTheDocument();
+    expect(btnSubmit).toBeDisabled();
+
+    userEvent.type(email, emailTest);
+    userEvent.type(password, passTest);
+
+    expect(btnSubmit).toBeEnabled();
+
+    userEvent.click(btnSubmit);
+
+    history.push('/meals');
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/meals');
+
+    const profileBtn = screen.getByTestId(idProfile);
+    expect(profileBtn).toBeInTheDocument();
+    userEvent.click(profileBtn);
+
     const logoutBtn = screen.getByTestId('profile-logout-btn');
     expect(logoutBtn).toBeInTheDocument();
 
-    userEvent.click(favoriteBtn);
+    userEvent.click(logoutBtn);
+    /* act(() => history.push('/')); */
+    const { location } = history;
+    expect(location.pathname).toBe('/');
   });
 });
