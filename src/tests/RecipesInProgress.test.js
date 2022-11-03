ddfb97d8877f09a21d274dc7e369251280669d25
Ester Viana Verza <ester.av@hotmail.com>
2022-11-03 18:13:08 -0300

    testes

--- src/tests/RecipesInProgress.test.js
+++ src/tests/RecipesInProgress.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
-import { saveDoneRecipe } from '../services/doneStorage';
 import App from '../App';
 
 const mockData = {
